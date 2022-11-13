07a4198e349609a8870d238fb4f2f070401baaab
Evorp <3vorpgaming@gmail.com>
2022-11-12 22:20:29 -0800

    way better config

--- config.py
+++ config.py
@@ -1,7 +1,21 @@
-DEFAULT_PREFIX = '~' # change this to change the default prefix if the server hasn't set one
-DEVELOPER = 'Evorp#5819' # idk why I included this here but who cares honestly
-EMBED_COLOR = 0xc3ba5c # this is just a hex color code (#C3BA5C) with 0x in front of it so discord parses it as hex, idk why either
-EMBED_ICON = 'https://raw.githubusercontent.com/3vorp/Spunch-Bot/main/assets/embed_icon.png' # frame 106 of the spongeboy gif, probably some better way to get the icon but this works too :P
+# change this to change the default prefix — if the server has set a prefix it will override this
+DEFAULT_PREFIX = '~'
+
+# used in feedback and help embeds for who to report or suggest stuff to
+DEVELOPER = 'Evorp#5819'
+
+# this is just a hex color code (#C3BA5C) with 0x in front of it so discord parses it as hex, as the name implies it's used on basically all embeds
+EMBED_COLOR = 0xc3ba5c
+
+# frame 106 of the spongeboy gif, probably some better way to get these icon but this works too :P
+EMBED_ICON = 'https://raw.githubusercontent.com/3vorp/Spunch-Bot/main/assets/embed_icon.png'
+
+# same thing but scaled up a ton (idk what filter was used)
 BIG_ICON = 'https://raw.githubusercontent.com/3vorp/Spunch-Bot/main/assets/big_icon.png'
+
+
+# the original spongeboy gif™
 EMBED_GIF = 'https://raw.githubusercontent.com/3vorp/Spunch-Bot/main/assets/embed_icon.gif'
+
+# used in info embeds
 BIG_GIF = 'https://raw.githubusercontent.com/3vorp/Spunch-Bot/main/assets/big_icon.gif'
