2a16911f6ffdbfd343c2b95de2d8b96fc9ed7ebe
Evorp <3vorpgaming@gmail.com>
2022-12-31 17:49:33 -0800

    last commit of 2022 lol (made config more helpful)

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 # change this to change the default prefix — if the server has set a prefix it will override this
 DEFAULT_PREFIX = '~'
 
+### image/embed stuff ###
+
 # this is just a hex color code (#C3BA5C) with 0x in front of it so discord parses it as hex
 EMBED_COLOR = 0xc3ba5c
 
@@ -16,8 +18,13 @@ EMBED_GIF = 'https://raw.githubusercontent.com/3vorp/Spunch-Bot/main/assets/embe
 # used in info embeds
 BIG_GIF = 'https://raw.githubusercontent.com/3vorp/Spunch-Bot/main/assets/big_icon.gif'
 
-# all of these are channel ids for various hardcoded dev-related things
-# (e.g startup messages, where to push global announcements from, where suggestions should go)
+### all of these are channel ids for various hardcoded dev-related things ###
+
+# for bot startup message
 STARTUP_CHANNEL = 1034609478005436436
+
+# for where suggestions go
 SUGGEST_CHANNEL = 1035020903953743942
+
+# for where announcements are pushed from
 ANNOUNCEMENT_CHANNEL = 1038693661824790598
