ae3f44e3f7d7dd80a875fce74a77ba0a594bd2bc
Evorp <3vorpgaming@gmail.com>
2023-01-13 21:42:49 -0800

    remove unused variables from config

--- README.md
+++ README.md
@@ -46,6 +46,8 @@ it just uses regular json formatting, read the example file for specifics
 
 `config.py` contains most of the hardcoded stuff like colors and channel ids, so feel free to edit that to change most defaults
 
+oh also if you're renaming the bot etc the frame of spongeboy used on static images is always 106
+
 # IF YOU ARE USING VISUAL STUDIO CODE (VSCODE) AND ARE USING THE CODE RUNNER EXTENSION:
 
 the default method that the Code Runner extension uses for running a python file is by running it straight from the path
--- config.py
+++ config.py
@@ -6,12 +6,6 @@ DEFAULT_PREFIX = '~'
 # this is just a hex color code (#C3BA5C) with 0x in front of it so discord parses it as hex
 EMBED_COLOR = 0xc3ba5c
 
-# frame 106 of the spongeboy gif, probably some better way to get these icon but this works too :P
-EMBED_ICON = 'https://raw.githubusercontent.com/3vorp/Spunch-Bot/main/assets/embed_icon.png'
-
-# same thing but scaled up a ton (idk what filter was used)
-BIG_ICON = 'https://raw.githubusercontent.com/3vorp/Spunch-Bot/main/assets/big_icon.png'
-
 # the original spongeboy gif™
 EMBED_GIF = 'https://raw.githubusercontent.com/3vorp/Spunch-Bot/main/assets/embed_icon.gif'
 
@@ -20,7 +14,7 @@ BIG_GIF = 'https://raw.githubusercontent.com/3vorp/Spunch-Bot/main/assets/big_ic
 
 ### all of these are channel ids for various hardcoded dev-related things ###
 
-# for bot startup message
+# for where the bot gives its startup message
 STARTUP_CHANNEL = 1034609478005436436
 
 # for where suggestions go
