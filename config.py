6b4eaae110dbe1cdaa7305a191e980201844c488
Evorp <3vorpgaming@gmail.com>
2023-01-13 21:45:37 -0800

    better config formatting

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
+### general branding stuff ####
+
+
+
 # change this to change the default prefix — if the server has set a prefix it will override this
 DEFAULT_PREFIX = '~'
-
-### image/embed stuff ###
 
 # this is just a hex color code (#C3BA5C) with 0x in front of it so discord parses it as hex
 EMBED_COLOR = 0xc3ba5c
@@ -12,7 +14,11 @@ EMBED_GIF = 'https://raw.githubusercontent.com/3vorp/Spunch-Bot/main/assets/embe
 # used in info embeds
 BIG_GIF = 'https://raw.githubusercontent.com/3vorp/Spunch-Bot/main/assets/big_icon.gif'
 
-### all of these are channel ids for various hardcoded dev-related things ###
+
+
+### dev related stuff ###
+
+
 
 # for where the bot gives its startup message
 STARTUP_CHANNEL = 1034609478005436436
